aabba536609d98ce984d1f6c15afb421b90a0e0a
Ashi5486 <ashishkoushal5486@email.com>
2025-11-20 00:05:41 +0530

    last update

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,8 @@ app.use(cors({
 }));
 app.use(express.json());
 
+const _dirname = path.resolve();
+
 // Debug: check that routes load
 console.log("✅ Loading /api/auth routes...");
 app.use("/api/auth", authRoutes);
@@ -31,12 +33,21 @@ app.use("/api/projects",projectRoutes);
 app.use("/api/users", userRoutes);
 app.use("/uploads", express.static(path.join(path.resolve(), "/uploads")));
 
-app.get("/", (req, res) => {
-  res.send("✅ Server is running and connected to MongoDB");
+// app.get("/", (req, res) => {
+//   res.send("✅ Server is running and connected to MongoDB");
+// });
+
+// Serve frontend
+app.use(express.static(path.join(_dirname, "frontend/dist")));
+
+// FIX: Express 5 wildcard route must be REGEX
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(_dirname, "frontend/dist/index.html"));
 });
 
-// Server port
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
 });
